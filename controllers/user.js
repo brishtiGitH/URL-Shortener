e2773397162ecac399dd4ccd8da679593420c075
Brishti Das <das.brishti342@gmail.com>
2024-07-11 17:42:24 +0530

    fixed node app crash upon invalid login issue

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const createNewUser = async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        res.render('signup', {
+        return res.render('signup', {
             error: 'All fields are required.'
         })
     }
@@ -12,15 +12,26 @@ const createNewUser = async (req, res) => {
         email,
         password
     })
-    res.redirect('/')
+    return res.redirect('/')
 }
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({ email, password })  //if i put wrong details, login page is rendered w error msg BUT Node app is crashing.
-    console.log('user', user)
-    if (!user) {
-        res.render('login', { isSignedUp: "Wrong email or password." })
+
+    try {
+        const user = await User.findOne({ email, password })  //if i put wrong details, login page is rendered w error msg BUT Node app is crashing.
+        //fixed by adding "return" keyword before sending response.
+        console.log('user', user)
+        if (!user) {
+            return res.render('login', { isSignedUp: "Wrong email or password." })
+        }
+
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+        res.render('login', { err: error })
     }
-    res.redirect('/')
+
+
+
 }
 module.exports = { createNewUser, loginUser }
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const { createNewUser, loginUser } = require('../controllers/user')
 
 router.post('/signup', createNewUser) //localhost:3000/user/signup
-router.post('/login', loginUser)
+router.post('/login', loginUser)  //localhost:3000/user/login
 module.exports = router;
